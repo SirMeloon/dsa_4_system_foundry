e3aa0069bc1a50f4065e570ccd657864980220d9
SirMeloon <schuchtandre@yahoo.de>
2026-07-24 15:22:00 +0200

    feat: Enhance actor sheet portrait handling and styling, update templates for improved image display

--- module/sheets/actor-sheet.mjs
+++ module/sheets/actor-sheet.mjs
@@ -45,6 +45,9 @@ export class dsaActorSheet extends ActorSheet {
     // Add the actor's data to context.data for easier access, as well as flags.
     context.system = actorData.system;
     context.flags = actorData.flags;
+    context.portrait = {
+      token: actorData.img === actorData.prototypeToken?.texture?.src,
+    };
 
     // Adding a pointer to CONFIG.DSA
     context.config = CONFIG.DSA;
